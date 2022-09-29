f1690777928dda5247dc4e6706e19b1dbb30d5d7
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-09-29 17:10:52 +0200

    [DF] Add RMaskedEntryRange
    A helper type that represents a range of entries with a mask.

--- tree/dataframe/inc/ROOT/RDF/RMaskedEntryRange.hxx
+++ tree/dataframe/inc/ROOT/RDF/RMaskedEntryRange.hxx
@@ -20,6 +20,9 @@ namespace ROOT {
 namespace Internal {
 namespace RDF {
 
+/// RDataFrame's internal representation of an entry range with a boolean mask.
+/// The mask has static size but depending on the dynamic bulk size fewer elements could be in use:
+/// do not take the size of the mask as the size of the bulk.
 class RMaskedEntryRange {
    ROOT::RVec<bool> fMask; ///< Boolean mask. Its size is set at construction time.
    Long64_t fBegin;        ///< Entry number of the first entry in the range this mask corresponds to.
@@ -37,18 +40,6 @@ public:
          fMask[i] |= other[i];
    }
    std::size_t Count(std::size_t until) const { return std::accumulate(fMask.begin(), fMask.begin() + until, 0ul); }
-
-   // Return std::numeric_limits<std::size_t>::max() if this mask is a superset of other.
-   // Otherwise return the index of the first entry in other that is not contained in this mask.
-   // Does _not_ check whether this->fBegin is equal to other.fBegin.
-   std::size_t Contains(const RMaskedEntryRange &other, std::size_t until)
-   {
-      for (std::size_t i = 0u; i < until; ++i)
-         if (other.fMask[i] && !fMask[i])
-            return i;
-
-      return std::numeric_limits<std::size_t>::max();
-   }
 };
 
 } // namespace RDF
