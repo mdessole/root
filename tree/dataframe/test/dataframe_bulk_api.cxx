53cd446d900abab9ce119f7139094a420e336666
Enrico Guiraud <enrico.guiraud@pm.me>
2023-06-26 16:12:13 -0600

    [DF] Add test for RAction and use-facing bulk API

--- tree/dataframe/test/dataframe_bulk_api.cxx
+++ tree/dataframe/test/dataframe_bulk_api.cxx
@@ -1,3 +1,4 @@
+#include "RtypesCore.h"
 #include <ROOT/RDataFrame.hxx>
 
 #include <gtest/gtest.h>
@@ -20,6 +21,36 @@ TEST(RDFBulkAPI, BulkDefine)
       ROOT::RDataFrame(10).Define("x", bulkReturnX, {"rdfentry_"}).Define("xx", bulkSqr, {"x"}).Mean<float>("xx");
    EXPECT_DOUBLE_EQ(m.GetValue(), 28.5);
 }
+
+
+struct BulkHelper : ROOT::Detail::RDF::RActionImpl<BulkHelper> {
+   static constexpr bool kUseBulk = true;
+   using Result_t = int;
+   std::shared_ptr<int> x = std::make_shared<int>(42);
+
+   std::shared_ptr<int> GetResultPtr() const { return x; }
+
+   void Exec(const ROOT::RDF::Experimental::REventMask &m, const ROOT::RVecULL& es) {
+      EXPECT_EQ(m.FirstEntry(), 0ull);
+      EXPECT_EQ(m.Size(), 10ull);
+      for (ULong64_t i = 0ull; i < 10ull; ++i)
+         EXPECT_EQ(i, es[i]);
+   }
+
+   void Initialize() {}
+   void InitTask(TTreeReader *, unsigned) {}
+   void Finalize() {}
+   std::string GetActionName() { return "custom"; }
+};
+
+
+TEST(RDFBulkAPI, BulkAction)
+{
+  auto df = ROOT::RDataFrame(10);
+  auto r = df.Book<ULong64_t>(BulkHelper{}, {"rdfentry_"});
+  r.GetValue();
+}
+
 
 /*
 TEST(RDFBulkAPI, BulkFilter)
