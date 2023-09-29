aca01e1f3f90e27eaee5cf9164f7e876e8da2cf6
mdessole <monicadessole@gmail.com>
2023-09-29 11:48:59 +0200

    removed duplicate of etaMax_impl

--- math/genvector/inc/Math/GenVector/PtEtaPhiM4D.h
+++ math/genvector/inc/Math/GenVector/PtEtaPhiM4D.h
@@ -2,11 +2,11 @@
 // Authors: W. Brown, M. Fischler, L. Moneta    2005
 
 /**********************************************************************
-*                                                                    *
-* Copyright (c) 2005 , LCG ROOT FNAL MathLib Team                    *
-*                                                                    *
-*                                                                    *
-**********************************************************************/
+ *                                                                    *
+ * Copyright (c) 2005 , LCG ROOT FNAL MathLib Team                    *
+ *                                                                    *
+ *                                                                    *
+ **********************************************************************/
 
 // Header file for class PtEtaPhiM4D
 //
@@ -16,18 +16,17 @@
 // Last update: $Id$
 //
 #ifndef ROOT_Math_GenVector_PtEtaPhiM4D
-#define ROOT_Math_GenVector_PtEtaPhiM4D  1
+#define ROOT_Math_GenVector_PtEtaPhiM4D 1
 
 #undef __MAKECINT__
 
 #include "Math/GenVector/etaMax.h"
 #include "Math/GenVector/MathUtil.h"
 
-//#define TRACE_CE
+// #define TRACE_CE
 #ifdef TRACE_CE
 #include <iostream>
 #endif
-
 
 namespace ROOT {
 
@@ -50,8 +49,7 @@ namespace Math {
 template <class ScalarType>
 class PtEtaPhiM4D {
 
-public :
-
+public:
    typedef ScalarType Scalar;
 
    // --------- Constructors ---------------
@@ -59,24 +57,27 @@ public :
    /**
       Default constructor gives zero 4-vector (with zero mass)
    */
-   PtEtaPhiM4D() : fPt(0), fEta(0), fPhi(0), fM(0) { }
+   PtEtaPhiM4D() : fPt(0), fEta(0), fPhi(0), fM(0) {}
 
    /**
       Constructor  from pt, eta, phi, mass values
    */
-   PtEtaPhiM4D(Scalar pt, Scalar eta, Scalar phi, Scalar mass) :
-      fPt(pt), fEta(eta), fPhi(phi), fM(mass) {
+   PtEtaPhiM4D(Scalar pt, Scalar eta, Scalar phi, Scalar mass) : fPt(pt), fEta(eta), fPhi(phi), fM(mass)
+   {
       RestrictPhi();
-      if (fM < 0) RestrictNegMass();
+      if (fM < 0)
+         RestrictNegMass();
    }
 
    /**
       Generic constructor from any 4D coordinate system implementing
       Pt(), Eta(), Phi() and M()
    */
-   template <class CoordSystem >
-   explicit PtEtaPhiM4D(const CoordSystem & c) :
-      fPt(c.Pt()), fEta(c.Eta()), fPhi(c.Phi()), fM(c.M())  { RestrictPhi(); }
+   template <class CoordSystem>
+   explicit PtEtaPhiM4D(const CoordSystem &c) : fPt(c.Pt()), fEta(c.Eta()), fPhi(c.Phi()), fM(c.M())
+   {
+      RestrictPhi();
+   }
 
    // for g++  3.2 and 3.4 on 32 bits found that the compiler generated copy ctor and assignment are much slower
    // so we decided to re-implement them ( there is no no need to have them with g++4)
@@ -84,87 +85,111 @@ public :
    /**
       copy constructor
     */
-   PtEtaPhiM4D(const PtEtaPhiM4D & v) :
-      fPt(v.fPt), fEta(v.fEta), fPhi(v.fPhi), fM(v.fM) { }
+   PtEtaPhiM4D(const PtEtaPhiM4D &v) : fPt(v.fPt), fEta(v.fEta), fPhi(v.fPhi), fM(v.fM) {}
 
    /**
       assignment operator
     */
-   PtEtaPhiM4D & operator = (const PtEtaPhiM4D & v) {
-      fPt  = v.fPt;
+   PtEtaPhiM4D &operator=(const PtEtaPhiM4D &v)
+   {
+      fPt = v.fPt;
       fEta = v.fEta;
       fPhi = v.fPhi;
-      fM   = v.fM;
+      fM = v.fM;
       return *this;
    }
-
 
    /**
       Set internal data based on an array of 4 Scalar numbers
    */
-   void SetCoordinates( const Scalar src[] ) {
-      fPt=src[0]; fEta=src[1]; fPhi=src[2]; fM=src[3];
+   void SetCoordinates(const Scalar src[])
+   {
+      fPt = src[0];
+      fEta = src[1];
+      fPhi = src[2];
+      fM = src[3];
       RestrictPhi();
-      if (fM <0) RestrictNegMass();
+      if (fM < 0)
+         RestrictNegMass();
    }
 
    /**
       get internal data into an array of 4 Scalar numbers
    */
-   void GetCoordinates( Scalar dest[] ) const
-   { dest[0] = fPt; dest[1] = fEta; dest[2] = fPhi; dest[3] = fM; }
+   void GetCoordinates(Scalar dest[]) const
+   {
+      dest[0] = fPt;
+      dest[1] = fEta;
+      dest[2] = fPhi;
+      dest[3] = fM;
+   }
 
    /**
       Set internal data based on 4 Scalar numbers
    */
-   void SetCoordinates(Scalar pt, Scalar eta, Scalar phi, Scalar mass) {
-      fPt=pt; fEta = eta; fPhi = phi; fM = mass;
+   void SetCoordinates(Scalar pt, Scalar eta, Scalar phi, Scalar mass)
+   {
+      fPt = pt;
+      fEta = eta;
+      fPhi = phi;
+      fM = mass;
       RestrictPhi();
-      if (fM <0) RestrictNegMass();
+      if (fM < 0)
+         RestrictNegMass();
    }
 
    /**
       get internal data into 4 Scalar numbers
    */
-   void
-   GetCoordinates(Scalar& pt, Scalar & eta, Scalar & phi, Scalar& mass) const
-   { pt=fPt; eta=fEta; phi = fPhi; mass = fM; }
+   void GetCoordinates(Scalar &pt, Scalar &eta, Scalar &phi, Scalar &mass) const
+   {
+      pt = fPt;
+      eta = fEta;
+      phi = fPhi;
+      mass = fM;
+   }
 
    // --------- Coordinates and Coordinate-like Scalar properties -------------
 
    // 4-D Cylindrical eta coordinate accessors
 
-   Scalar Pt()  const { return fPt;  }
+   Scalar Pt() const { return fPt; }
    Scalar Eta() const { return fEta; }
    Scalar Phi() const { return fPhi; }
    /**
        M() is the invariant mass;
        in this coordinate system it can be negagative if set that way.
    */
-   Scalar M()   const { return fM;   }
+   Scalar M() const { return fM; }
    Scalar Mag() const { return M(); }
 
-   Scalar Perp()const { return Pt(); }
+   Scalar Perp() const { return Pt(); }
    Scalar Rho() const { return Pt(); }
 
    // other coordinate representation
 
    Scalar Px() const { return fPt * mycos(fPhi); }
-   Scalar X () const { return Px();         }
+   Scalar X() const { return Px(); }
    Scalar Py() const { return fPt * mysin(fPhi); }
-   Scalar Y () const { return Py();         }
-   Scalar Pz() const {
-      return fPt > 0 ? fPt * mysinh(fEta) : fEta == 0 ? 0 : fEta > 0 ? fEta - etaMax<Scalar>() : fEta + etaMax<Scalar>();
+   Scalar Y() const { return Py(); }
+   Scalar Pz() const
+   {
+      return fPt > 0     ? fPt * mysinh(fEta)
+             : fEta == 0 ? 0
+             : fEta > 0  ? fEta - etaMax<Scalar>()
+                         : fEta + etaMax<Scalar>();
    }
-   Scalar Z () const { return Pz(); }
+   Scalar Z() const { return Pz(); }
 
    /**
        magnitude of momentum
    */
-   Scalar P() const {
-      return fPt > 0 ? fPt * mycosh(fEta)
-                     : fEta > etaMax<Scalar>() ? fEta - etaMax<Scalar>()
-                                               : fEta < -etaMax<Scalar>() ? -fEta - etaMax<Scalar>() : 0;
+   Scalar P() const
+   {
+      return fPt > 0                    ? fPt * mycosh(fEta)
+             : fEta > etaMax<Scalar>()  ? fEta - etaMax<Scalar>()
+             : fEta < -etaMax<Scalar>() ? -fEta - etaMax<Scalar>()
+                                        : 0;
    }
    Scalar R() const { return P(); }
 
@@ -180,8 +205,9 @@ public :
    /**
        energy squared
    */
-   Scalar E2() const {
-      Scalar e2 =  P2() + M2();
+   Scalar E2() const
+   {
+      Scalar e2 = P2() + M2();
       // avoid rounding error which can make E2 negative when M2 is negative
       return e2 > 0 ? e2 : 0;
    }
@@ -191,38 +217,37 @@ public :
    */
    Scalar E() const { return mysqrt(E2()); }
 
-   Scalar T()   const { return E();  }
+   Scalar T() const { return E(); }
 
    /**
       vector magnitude squared (or mass squared)
       In case of negative mass (spacelike particles return negative values)
    */
-   Scalar M2() const   {
-      return ( fM  >= 0 ) ?  fM*fM :  -fM*fM;
-   }
-   Scalar Mag2() const { return M2();  }
+   Scalar M2() const { return (fM >= 0) ? fM * fM : -fM * fM; }
+   Scalar Mag2() const { return M2(); }
 
    /**
        transverse spatial component squared
    */
-   Scalar Pt2()   const { return fPt*fPt;}
-   Scalar Perp2() const { return Pt2();  }
+   Scalar Pt2() const { return fPt * fPt; }
+   Scalar Perp2() const { return Pt2(); }
 
    /**
        transverse mass squared
    */
-   Scalar Mt2() const { return M2()  + fPt*fPt; }
+   Scalar Mt2() const { return M2() + fPt * fPt; }
 
    /**
       transverse mass - will be negative if Mt2() is negative
    */
-   Scalar Mt() const {
+   Scalar Mt() const
+   {
       const Scalar mm = Mt2();
       if (mm >= 0) {
          return mysqrt(mm);
       } else {
-         //GenVector::Throw  ("PtEtaPhiM4D::Mt() - Tachyonic:\n"
-         //                   "    Pz^2 > E^2 so the transverse mass would be imaginary");
+         // GenVector::Throw  ("PtEtaPhiM4D::Mt() - Tachyonic:\n"
+         //                    "    Pz^2 > E^2 so the transverse mass would be imaginary");
          return -mysqrt(-mm);
       }
    }
@@ -230,7 +255,8 @@ public :
    /**
        transverse energy squared
    */
-   Scalar Et2() const {
+   Scalar Et2() const
+   {
       // a bit faster than et * et
       return 2. * E2() / (mycosh(2 * fEta) + 1);
    }
@@ -242,22 +268,24 @@ public :
 
 private:
    inline static Scalar pi() { return M_PI; }
-   inline void RestrictPhi() {
-      if (fPhi <= -pi() || fPhi > pi()) fPhi = fPhi - myfloor(fPhi / (2 * pi()) + .5) * 2 * pi();
+   inline void RestrictPhi()
+   {
+      if (fPhi <= -pi() || fPhi > pi())
+         fPhi = fPhi - myfloor(fPhi / (2 * pi()) + .5) * 2 * pi();
    }
    // restrict the value of negative mass to avoid unphysical negative E2 values
    // M2 must be less than P2 for the tachionic particles - otherwise use positive values
-   inline void RestrictNegMass() {
+   inline void RestrictNegMass()
+   {
       if (fM < 0) {
          if (P2() - fM * fM < 0) {
-            //GenVector::Throw("PtEtaPhiM4D::unphysical value of mass, set to closest physical value");
+            // GenVector::Throw("PtEtaPhiM4D::unphysical value of mass, set to closest physical value");
             fM = -P();
          }
       }
    }
 
 public:
-
    /**
       polar angle
    */
@@ -268,35 +296,33 @@ public:
    /**
       set Pt value
    */
-   void SetPt( Scalar  pt) {
-      fPt = pt;
-   }
+   void SetPt(Scalar pt) { fPt = pt; }
    /**
       set eta value
    */
-   void SetEta( Scalar  eta) {
-      fEta = eta;
-   }
+   void SetEta(Scalar eta) { fEta = eta; }
    /**
       set phi value
    */
-   void SetPhi( Scalar  phi) {
+   void SetPhi(Scalar phi)
+   {
       fPhi = phi;
       RestrictPhi();
    }
    /**
       set M value
    */
-   void SetM( Scalar  mass) {
+   void SetM(Scalar mass)
+   {
       fM = mass;
-      if (fM <0) RestrictNegMass();
+      if (fM < 0)
+         RestrictNegMass();
    }
 
    /**
        set values using cartesian coordinate system
    */
    void SetPxPyPzE(Scalar px, Scalar py, Scalar pz, Scalar e);
-
 
    // ------ Manipulations -------------
 
@@ -305,44 +331,48 @@ public:
       therefore negate will work only on the spatial components
       One would need to use negate only with vectors having the energy as data members
    */
-   void Negate( ) {
-      fPhi = ( (fPhi > 0) ? fPhi - pi() : fPhi + pi()  );
-      fEta = - fEta;
-      //GenVector::Throw ("PtEtaPhiM4D::Negate - cannot negate the energy - can negate only the spatial components");
+   void Negate()
+   {
+      fPhi = ((fPhi > 0) ? fPhi - pi() : fPhi + pi());
+      fEta = -fEta;
+      // GenVector::Throw ("PtEtaPhiM4D::Negate - cannot negate the energy - can negate only the spatial components");
    }
 
    /**
       Scale coordinate values by a scalar quantity a
    */
-   void Scale( Scalar a) {
+   void Scale(Scalar a)
+   {
       if (a < 0) {
-         Negate(); a = -a;
+         Negate();
+         a = -a;
       }
       fPt *= a;
-      fM  *= a;
+      fM *= a;
    }
 
    /**
       Assignment from a generic coordinate system implementing
       Pt(), Eta(), Phi() and M()
    */
-   template <class CoordSystem >
-   PtEtaPhiM4D & operator = (const CoordSystem & c) {
-      fPt  = c.Pt();
+   template <class CoordSystem>
+   PtEtaPhiM4D &operator=(const CoordSystem &c)
+   {
+      fPt = c.Pt();
       fEta = c.Eta();
       fPhi = c.Phi();
-      fM   = c.M();
+      fM = c.M();
       return *this;
    }
 
    /**
       Exact equality
    */
-   bool operator == (const PtEtaPhiM4D & rhs) const {
-      return fPt == rhs.fPt && fEta == rhs.fEta
-         && fPhi == rhs.fPhi && fM == rhs.fM;
+   bool operator==(const PtEtaPhiM4D &rhs) const
+   {
+      return fPt == rhs.fPt && fEta == rhs.fEta && fPhi == rhs.fPhi && fM == rhs.fM;
    }
-   bool operator != (const PtEtaPhiM4D & rhs) const {return !(operator==(rhs));}
+   bool operator!=(const PtEtaPhiM4D &rhs) const { return !(operator==(rhs)); }
 
    // ============= Compatibility section ==================
 
@@ -352,7 +382,6 @@ public:
    Scalar y() const { return Y(); }
    Scalar z() const { return Z(); }
    Scalar t() const { return E(); }
-
 
 #if defined(__MAKECINT__) || defined(G__DICTIONARY)
 
@@ -369,18 +398,14 @@ public:
 #endif
 
 private:
-
    ScalarType fPt;
    ScalarType fEta;
    ScalarType fPhi;
    ScalarType fM;
-
 };
-
 
 } // end namespace Math
 } // end namespace ROOT
-
 
 // move implementations here to avoid circle dependencies
 #include "Math/GenVector/PxPyPzE4D.h"
@@ -393,48 +418,57 @@ namespace ROOT {
 
 namespace Math {
 
-
 template <class ScalarType>
-inline void PtEtaPhiM4D<ScalarType>::SetPxPyPzE(Scalar px, Scalar py, Scalar pz, Scalar e) {
-   *this = PxPyPzE4D<Scalar> (px, py, pz, e);
+inline void PtEtaPhiM4D<ScalarType>::SetPxPyPzE(Scalar px, Scalar py, Scalar pz, Scalar e)
+{
+   *this = PxPyPzE4D<Scalar>(px, py, pz, e);
 }
-
 
 #if defined(__MAKECINT__) || defined(G__DICTIONARY)
 
-  // ====== Set member functions for coordinates in other systems =======
+// ====== Set member functions for coordinates in other systems =======
 
 template <class ScalarType>
-void PtEtaPhiM4D<ScalarType>::SetPx(Scalar px) {
+void PtEtaPhiM4D<ScalarType>::SetPx(Scalar px)
+{
    GenVector_exception e("PtEtaPhiM4D::SetPx() is not supposed to be called");
    throw e;
-   PxPyPzE4D<Scalar> v(*this); v.SetPx(px); *this = PtEtaPhiM4D<Scalar>(v);
+   PxPyPzE4D<Scalar> v(*this);
+   v.SetPx(px);
+   *this = PtEtaPhiM4D<Scalar>(v);
 }
 template <class ScalarType>
-void PtEtaPhiM4D<ScalarType>::SetPy(Scalar py) {
+void PtEtaPhiM4D<ScalarType>::SetPy(Scalar py)
+{
    GenVector_exception e("PtEtaPhiM4D::SetPx() is not supposed to be called");
    throw e;
-   PxPyPzE4D<Scalar> v(*this); v.SetPy(py); *this = PtEtaPhiM4D<Scalar>(v);
+   PxPyPzE4D<Scalar> v(*this);
+   v.SetPy(py);
+   *this = PtEtaPhiM4D<Scalar>(v);
 }
 template <class ScalarType>
-void PtEtaPhiM4D<ScalarType>::SetPz(Scalar pz) {
+void PtEtaPhiM4D<ScalarType>::SetPz(Scalar pz)
+{
    GenVector_exception e("PtEtaPhiM4D::SetPx() is not supposed to be called");
    throw e;
-   PxPyPzE4D<Scalar> v(*this); v.SetPz(pz); *this = PtEtaPhiM4D<Scalar>(v);
+   PxPyPzE4D<Scalar> v(*this);
+   v.SetPz(pz);
+   *this = PtEtaPhiM4D<Scalar>(v);
 }
 template <class ScalarType>
-void PtEtaPhiM4D<ScalarType>::SetE(Scalar energy) {
+void PtEtaPhiM4D<ScalarType>::SetE(Scalar energy)
+{
    GenVector_exception e("PtEtaPhiM4D::SetE() is not supposed to be called");
    throw e;
-   PxPyPzE4D<Scalar> v(*this); v.SetE(energy);   *this = PtEtaPhiM4D<Scalar>(v);
+   PxPyPzE4D<Scalar> v(*this);
+   v.SetE(energy);
+   *this = PtEtaPhiM4D<Scalar>(v);
 }
 
-#endif  // endif __MAKE__CINT || G__DICTIONARY
+#endif // endif __MAKE__CINT || G__DICTIONARY
 
 } // end namespace Math
 
 } // end namespace ROOT
-
-
 
 #endif // ROOT_Math_GenVector_PtEtaPhiM4D
--- math/genvector/inc/Math/GenVector/etaMax.h
+++ math/genvector/inc/Math/GenVector/etaMax.h
@@ -1,71 +1,54 @@
 // @(#)root/mathcore:$Id$
 // Authors: W. Brown, M. Fischler, L. Moneta    2005
 
- /**********************************************************************
-  *                                                                    *
-  * Copyright (c) 2005 , FNAL MathLib Team                             *
-  *                                                                    *
-  *                                                                    *
-  **********************************************************************/
-
+/**********************************************************************
+ *                                                                    *
+ * Copyright (c) 2005 , FNAL MathLib Team                             *
+ *                                                                    *
+ *                                                                    *
+ **********************************************************************/
 
 // Header source file for function etaMax
 //
 // Created by: Mark Fischler  at Thu Jun 2 2005
 
-
 #ifndef ROOT_Math_GenVector_etaMax
-#define ROOT_Math_GenVector_etaMax  1
+#define ROOT_Math_GenVector_etaMax 1
 
 #include "Math/GenVector/MathUtil.h"
 
 #include <limits>
 #include <cmath>
 
-
 namespace ROOT {
 
-  namespace Math {
+namespace Math {
 
-    /**
-        The following function could be called to provide the maximum possible
-        value of pseudorapidity for a non-zero rho.  This is log ( max/min )
-        where max and min are the extrema of positive values for type
-        long double.
-     */
-#ifdef ROOT_MATH_SYCL 
-    inline
-     double etaMax_impl() {
-      return mylog ( std::numeric_limits< double>::max()/256.0 ) -
-             mylog ( std::numeric_limits< double>::denorm_min()*256.0 )
-             + 16.0 * log(2.0);
-    // Actual usage of etaMax() simply returns the number 22756, which is
-    // the answer this would supply, rounded to a higher integer.
-    }
-#else
-    inline
-     long double etaMax_impl() {
-      return mylog ( std::numeric_limits<long double>::max()/256.0 ) -
-             mylog ( std::numeric_limits<long double>::denorm_min()*256.0 )
-             + 16.0 * log(2.0);
-    // Actual usage of etaMax() simply returns the number 22756, which is
-    // the answer this would supply, rounded to a higher integer.
-    }
-#endif
+/**
+    The following function could be called to provide the maximum possible
+    value of pseudorapidity for a non-zero rho.  This is log ( max/min )
+    where max and min are the extrema of positive values for type
+    long double.
+ */
+inline long double etaMax_impl()
+{
+   return mylog(std::numeric_limits<double>::max() / 256.0) - mylog(std::numeric_limits<double>::denorm_min() * 256.0) +
+          16.0 * log(2.0);
+   // Actual usage of etaMax() simply returns the number 22756, which is
+   // the answer this would supply, rounded to a higher integer.
+}
+/**
+    Function providing the maximum possible value of pseudorapidity for
+    a non-zero rho, in the Scalar type with the largest dynamic range.
+ */
+template <class T>
+inline T etaMax()
+{
+   return static_cast<T>(22756.0);
+}
 
-    /**
-        Function providing the maximum possible value of pseudorapidity for
-        a non-zero rho, in the Scalar type with the largest dynamic range.
-     */
-    template <class T>
-    inline
-    T etaMax() {
-      return static_cast<T>(22756.0);
-    }
-
-  } // namespace Math
+} // namespace Math
 
 } // namespace ROOT
-
 
 #endif /* ROOT_Math_GenVector_etaMax  */
