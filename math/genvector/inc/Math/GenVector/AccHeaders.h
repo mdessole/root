7eb2b271c44eddbf2d50276651e15a6f0e6d0f5c
mdessole <monicadessole@gmail.com>
2024-04-17 16:26:57 +0200

    Rearrange ifdefs

--- math/genvector/inc/Math/GenVector/AccHeaders.h
+++ math/genvector/inc/Math/GenVector/AccHeaders.h
@@ -4,26 +4,31 @@
 #if defined(ROOT_MATH_SYCL)
 
 #include <sycl/sycl.hpp>
-#elif defined(ROOT_MATH_CUDA)
-#include <math.h>
-#else
-#include <cmath>
-
 #ifndef ROOT_MATH_ARCH
 #define ROOT_MATH_ARCH MathSYCL
-#endif
+#endif // ROOT_MATH_ARCH
 
-#endif
+#elif defined(ROOT_MATH_CUDA)
+
+#include <math.h>
+#ifndef ROOT_MATH_ARCH
+#define ROOT_MATH_ARCH MathCUDA
+#endif // ROOT_MATH_ARCH
+
+#else
+
+#include <cmath>
+#ifndef ROOT_MATH_ARCH
+#define ROOT_MATH_ARCH Math
+#endif // ROOT_MATH_ARCH
+
+#endif 
 
 #if defined(ROOT_MATH_CUDA) && defined(__CUDACC__)
 
 #define __roodevice__ __device__
 #define __roohost__ __host__
 #define __rooglobal__ __global__
-
-#ifndef ROOT_MATH_ARCH
-#define ROOT_MATH_ARCH MathCUDA
-#endif
 
 #else
 
@@ -33,12 +38,5 @@
 
 #endif
 
-#if !defined(ROOT_MATH_SYCL) && !defined(ROOT_MATH_CUDA)
-
-#ifndef ROOT_MATH_ARCH
-#define ROOT_MATH_ARCH Math
-#endif
-
-#endif
 
 #endif // ROOT_AccHeaders_H
