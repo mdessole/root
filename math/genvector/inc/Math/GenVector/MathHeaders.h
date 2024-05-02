0e1717b8fce2406917dd410c502ea1b82eb67f42
mdessole <monicadessole@gmail.com>
2024-05-02 15:16:10 +0200

    Remove extra Pi definition

--- math/genvector/inc/Math/GenVector/MathHeaders.h
+++ math/genvector/inc/Math/GenVector/MathHeaders.h
@@ -13,17 +13,6 @@
 #ifndef ROOT_MathHeaders_H
 #define ROOT_MathHeaders_H
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846264338328 // Pi
-#endif
-
-#ifndef M_PI_2
-#define M_PI_2 1.57079632679489661923132169164 // Pi/2
-#endif
-
-#ifndef M_PI_4
-#define M_PI_4 0.78539816339744830961566084582 // Pi/4
-#endif
 
 #include "Math/GenVector/AccHeaders.h"
 
